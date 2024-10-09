05272bf34edf4969c94f5891870b14f4918679d5
ASelezniov <seleznev.a@clinicnacpp.ru>
2024-10-09 17:05:16 +0300

    first commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path, re_path
+from django.contrib.sitemaps.views import sitemap
+
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    "posts": PostSitemap,
+}
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
-]
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls", namespace="blog")),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
